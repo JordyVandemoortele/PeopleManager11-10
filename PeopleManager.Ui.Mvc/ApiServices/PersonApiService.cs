08267ba540d7f3c5649ea4304b447bd26c9147bd
JordyVandemoortele <125571637+JordyVandemoortele@users.noreply.github.com>
2023-10-11 13:38:23 +0200

    Initial commit

---
+++ PeopleManager.Ui.Mvc/ApiServices/PersonApiService.cs
@@ -0,0 +1,99 @@
+﻿using PeopleManager.Dto.Requests;
+using PeopleManager.Dto.Results;
+using PeopleManager.Ui.Mvc.ApiServices.Extensions;
+using PeopleManager.Ui.Mvc.Stores;
+using Vives.Services.Model;
+
+namespace PeopleManager.Ui.Mvc.ApiServices
+{
+    public class PersonApiService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly TokenStore _tokenStore;
+
+        public PersonApiService(
+            IHttpClientFactory httpClientFactory,
+            TokenStore tokenStore)
+        {
+            _httpClientFactory = httpClientFactory;
+            _tokenStore = tokenStore;
+        }
+
+        public async Task<IList<PersonResult>> Find()
+        {
+            var httpClient = _httpClientFactory.CreateClient("PeopleManagerApi");
+            httpClient.AddAuthorization(_tokenStore.GetToken());
+            var route = "/api/people";
+            var httpResponse = await httpClient.GetAsync(route);
+
+            httpResponse.EnsureSuccessStatusCode();
+
+            var people = await httpResponse.Content.ReadFromJsonAsync<IList<PersonResult>>();
+
+            return people ?? new List<PersonResult>();
+        }
+
+        public async Task<PersonResult?> Get(int id)
+        {
+            var httpClient = _httpClientFactory.CreateClient("PeopleManagerApi");
+            httpClient.AddAuthorization(_tokenStore.GetToken());
+            var route = $"/api/people/{id}";
+            var httpResponse = await httpClient.GetAsync(route);
+
+            httpResponse.EnsureSuccessStatusCode();
+
+            return await httpResponse.Content.ReadFromJsonAsync<PersonResult>();
+        }
+
+        public async Task<ServiceResult<PersonResult?>> Create(PersonRequest person)
+        {
+            var httpClient = _httpClientFactory.CreateClient("PeopleManagerApi");
+            httpClient.AddAuthorization(_tokenStore.GetToken());
+            var route = "/api/people";
+            var httpResponse = await httpClient.PostAsJsonAsync(route, person);
+
+            httpResponse.EnsureSuccessStatusCode();
+
+            var serviceResult = await httpResponse.Content.ReadFromJsonAsync<ServiceResult<PersonResult?>>();
+            if (serviceResult is null)
+            {
+                return new ServiceResult<PersonResult?>().ApiError();
+            }
+            return serviceResult;
+        }
+
+        public async Task<ServiceResult<PersonResult?>> Update(int id, PersonRequest person)
+        {
+            var httpClient = _httpClientFactory.CreateClient("PeopleManagerApi");
+            httpClient.AddAuthorization(_tokenStore.GetToken());
+            var route = $"/api/people/{id}";
+            var httpResponse = await httpClient.PutAsJsonAsync(route, person);
+
+            httpResponse.EnsureSuccessStatusCode();
+
+            var serviceResult = await httpResponse.Content.ReadFromJsonAsync<ServiceResult<PersonResult?>>();
+            if (serviceResult is null)
+            {
+                return new ServiceResult<PersonResult?>().ApiError();
+            }
+            return serviceResult;
+        }
+
+        public async Task<ServiceResult> Delete(int id)
+        {
+            var httpClient = _httpClientFactory.CreateClient("PeopleManagerApi");
+            httpClient.AddAuthorization(_tokenStore.GetToken());
+            var route = $"/api/people/{id}";
+            var httpResponse = await httpClient.DeleteAsync(route);
+
+            httpResponse.EnsureSuccessStatusCode();
+
+            var serviceResult = await httpResponse.Content.ReadFromJsonAsync<ServiceResult>();
+            if (serviceResult is null)
+            {
+                return new ServiceResult().ApiError();
+            }
+            return serviceResult;
+        }
+    }
+}
---
+++ PeopleManager.Ui.Mvc/Stores/TokenStore.cs
@@ -0,0 +1,24 @@
+﻿namespace PeopleManager.Ui.Mvc.Stores
+{
+    public class TokenStore
+    {
+        private readonly IHttpContextAccessor _contextAccessor;
+
+        public TokenStore(IHttpContextAccessor contextAccessor)
+        {
+            _contextAccessor = contextAccessor;
+        }
+        public string? GetToken()
+        {
+            string? jwtToken = null;
+            _contextAccessor.HttpContext?.Request.Cookies.TryGetValue("JwtToken", out jwtToken);
+            return jwtToken;
+        }
+
+        public void SaveToken(string token)
+        {
+            _contextAccessor.HttpContext?.Response.Cookies.Delete("JwtToken");
+            _contextAccessor.HttpContext?.Response.Cookies.Append("JwtToken", token, new CookieOptions{HttpOnly = true});
+        }
+    }
+}
